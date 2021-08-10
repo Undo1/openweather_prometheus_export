d0f9e2cfd67ad527ddb3c33097caf25164b8e105
Undo1 <parker@erwaysoftware.com>
2021-08-09 22:57:37 -0600

    Initial Commit

---
+++ exporter.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	owm "github.com/briandowns/openweathermap"
+)
+
+var apiKey = os.Getenv("OWM_API_KEY")
+var zipCode, _ = strconv.Atoi(os.Getenv("OWM_ZIP_CODE"))
+
+func main() {
+	http.HandleFunc("/", func(respW http.ResponseWriter, r *http.Request) {
+		w, err := owm.NewCurrent("F", "en", apiKey) // fahrenheit (imperial) with Russian output
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		err = w.CurrentByZip(zipCode, "US")
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		temp := w.Main.Temp
+		pressure := w.Main.GrndLevel
+		humidity := w.Main.Humidity
+
+		rainLastHour := w.Rain.OneH
+		cloudiness := w.Clouds.All
+		windSpeed := w.Wind.Speed
+		windDir := w.Wind.Deg
+
+		fmt.Fprintf(respW, `
+owm_temperature %f
+owm_pressure_grnd %f
+owm_humidity %d
+owm_rain %f
+owm_cloudiness %d
+owm_wind_speed %f
+owm_wind_dir %f
+`, temp, pressure, humidity, rainLastHour, cloudiness, windSpeed, windDir)
+	})
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module openweather_prometheus_export
+
+go 1.16
+
+require github.com/briandowns/openweathermap v0.16.0
