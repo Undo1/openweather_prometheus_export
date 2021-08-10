e3311d4aeb6fa6ccd944a1549a7106304654835a
Undo1 <parker@erwaysoftware.com>
2021-08-09 22:59:09 -0600

    Tabs be gone

--- exporter.go
+++ exporter.go
@@ -1,40 +1,40 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-	"strconv"
+    "fmt"
+    "log"
+    "net/http"
+    "os"
+    "strconv"
 
-	owm "github.com/briandowns/openweathermap"
+    owm "github.com/briandowns/openweathermap"
 )
 
 var apiKey = os.Getenv("OWM_API_KEY")
 var zipCode, _ = strconv.Atoi(os.Getenv("OWM_ZIP_CODE"))
 
 func main() {
-	http.HandleFunc("/", func(respW http.ResponseWriter, r *http.Request) {
-		w, err := owm.NewCurrent("F", "en", apiKey) // fahrenheit (imperial) with Russian output
-		if err != nil {
-			log.Fatalln(err)
-		}
+    http.HandleFunc("/", func(respW http.ResponseWriter, r *http.Request) {
+        w, err := owm.NewCurrent("F", "en", apiKey) // fahrenheit (imperial) with Russian output
+        if err != nil {
+            log.Fatalln(err)
+        }
 
-		err = w.CurrentByZip(zipCode, "US")
-		if err != nil {
-			log.Fatalln(err)
-		}
+        err = w.CurrentByZip(zipCode, "US")
+        if err != nil {
+            log.Fatalln(err)
+        }
 
-		temp := w.Main.Temp
-		pressure := w.Main.GrndLevel
-		humidity := w.Main.Humidity
+        temp := w.Main.Temp
+        pressure := w.Main.GrndLevel
+        humidity := w.Main.Humidity
 
-		rainLastHour := w.Rain.OneH
-		cloudiness := w.Clouds.All
-		windSpeed := w.Wind.Speed
-		windDir := w.Wind.Deg
+        rainLastHour := w.Rain.OneH
+        cloudiness := w.Clouds.All
+        windSpeed := w.Wind.Speed
+        windDir := w.Wind.Deg
 
-		fmt.Fprintf(respW, `
+        fmt.Fprintf(respW, `
 owm_temperature %f
 owm_pressure_grnd %f
 owm_humidity %d
@@ -43,7 +43,7 @@ owm_cloudiness %d
 owm_wind_speed %f
 owm_wind_dir %f
 `, temp, pressure, humidity, rainLastHour, cloudiness, windSpeed, windDir)
-	})
+    })
 
-	log.Fatal(http.ListenAndServe(":8081", nil))
+    log.Fatal(http.ListenAndServe(":8081", nil))
 }
